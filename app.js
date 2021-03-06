dca43c74733c3f7fbea6c6ad3d1ce7d445192d59
Oscar <oscar.a.chavez.jr@gmail.com>
2021-03-06 13:10:40 -0700

    added dental touch and groupie projects

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ app.get('/', function(req, res){
     res.render("index.html")
 }) 
 
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`App is running on port ${ PORT }`);
